ef0cab74f35bfbc8dad17b88f75e271dd5759160
philippeeyraud <philippe.eyraud@ymail.com>
2022-03-08 08:14:33 +0100

    affichage des images sur la page d'accueil

---
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -0,0 +1,66 @@
+//Aller chercher des requêtes vers l'API .
+let outputData = [];
+
+const fetchOutput = async () => {
+await fetch("http://localhost:3000/api/products")
+//attendre la  recherche de la requête.
+
+//création d'une promise , on traite la réponse avec json.
+   .then((Response) => Response.json())
+//traiter la promise et récupérer le tableau   
+   .then ((Promise) => {outputData = Promise
+   console.log(outputData)}
+   )
+};
+//Avant d'executer la fonction on attend la réponse de fetchOutput.
+const outputDisplay = async () => {
+await fetchOutput();
+//on fait une boucle for
+//on va chercher les élément dans le DOM.
+//on cherche la source dans le back-end.
+// for (let v of outputData ) {document.getElementById("items").innerHTML+=`<article><img class="items" src="${v.imageUrl}" alt="image items du site" /></article>`;}
+const items = document.querySelector ('#items');
+const a = document. createElement(`a`);
+const aHref = document. createAttribute(`href`);
+aHref.value =`http://localhost:3000/api/products`;
+a.setAttributeNode(aHref);
+
+const article = document.createElement(`article`);
+
+const image = document.createElement(`img`);
+const imageSrc = document.createAttribute(`src`);
+imageSrc.value = `http://localhost:3000/images/kanap01.jpeg`;
+image.setAttributeNode(imageSrc);
+
+const subtitle = document.createElement(`h3`);
+const subtitleClass = document.createAttribute(`class`);
+subtitleClass.value = `productName`;
+const subtitletext = document.createTextNode(`Kanap name1`)
+subtitle.setAttributeNode(subtitleClass);
+
+const paragraphe = document.createElement(`p`);
+const paragrapheClass = document.createAttribute(`class`);
+paragrapheClass.value = `productDescription`
+const paragrapheText = document.createTextNode(`Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.`)
+paragraphe.setAttributeNode(paragrapheClass);
+
+items.appendChild(a);
+a.append(article);   
+article.append(image,subtitle,paragraphe);
+paragraphe.append(paragrapheText);
+subtitle.append(subtitletext);
+
+
+
+
+
+
+}
+//permet d'appeler la fonction
+outputDisplay();
+
+
+
+
+
+
