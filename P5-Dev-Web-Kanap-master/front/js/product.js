e47e7a4d77f134911d1a88068ddb4c7cfdd43825
philippeeyraud <philippe.eyraud@ymail.com>
2022-03-28 13:18:25 +0200

    création des liens

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,7 +1,15 @@
+
+
 let id = new URL(window.location.href).searchParams.get(`id`);
+console.log("mon id =" + id);
 
 
+//Aller chercher des requêtes vers l'API 
+let url = `http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926`;
 
-console.log("mon id ="+ id);
 
-
+fetch (url).then ((response) =>
+response.json().then((data) =>{
+console.log(data);
+})
+);
