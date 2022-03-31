97cc4e82f758a54180c796277c304cd971276315
philippeeyraud <philippe.eyraud@ymail.com>
2022-03-31 18:11:11 +0200

    données produit affichées, il faut afficher les couleurs

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -9,26 +9,32 @@ let url = `http://localhost:3000/api/products/${id}`;
 
 
 fetch(url).then((response) =>
-    response.json().then((promise) => {
-        id = promise;
-        console.log(promise);
-        const idDisplay = async () => {
-            const response = await fetch(url);
-            const {name,price, imageUrl, description,alttx} = id
-            console.log(imageUrl);
-            document.getElementById(`title`).textContent = name;
-            document.getElementById(`price`).textContent = price;
-            document.getElementById(`description`).textContent = description;
-            const image = document.createElement(`img`);
-            image.getElementsByTagName(`imgSrc`).document = imageUrl;
-            
-           
-        }
-        
-idDisplay()
+    response.json().then((product) => {
 
 
+        console.log(product);
+
+
+        const { name, price, imageUrl, description, alt } = product;
+
+        document.getElementById(`title`).textContent = name;
+        document.getElementById(`price`).textContent = price;
+        document.getElementById(`description`).textContent = description;
+
+
+        const image = document.createElement(`img`);
+        image.setAttribute(`src`,imageUrl);
+       
+      
+        const displayimg = document.querySelector(".item__img");
+        displayimg.appendChild (image);
     }
+
+
+
+
+
+
 
     ));
 
