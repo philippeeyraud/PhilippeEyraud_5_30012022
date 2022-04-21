0c115bf03a4a7482d86b7ae913f861b248d2befa
philippeeyraud <philippe.eyraud@ymail.com>
2022-04-21 09:47:08 +0200

    cart js

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,8 +1,11 @@
 
 //Récupérer les données du localstorage
 let cart = JSON.parse(localStorage.getItem("cart"));
-for (let v of cart) {
-    const id = v.id;
+
+for (let productObj of cart) {
+    const id = productObj.id;
+    const productColor = productObj.color;
+    const productQuantity = productObj.quantity;
     const url = `http://localhost:3000/api/products/${id}`
 
     fetch(url).then((response) =>
@@ -22,18 +25,33 @@ for (let v of cart) {
             const cartItem = document.createElement(`div`);
             const cartItemContentDescription = document.createElement(`div`);
             const displayh2 = document.querySelector(`.cart__item__content__description`)
+            const displayp = document.querySelector(`.cart__item__content__description`)
             const title = document.createElement(`h2`);
             const titleClass = document.createAttribute(`class`);
             titleClass.value = `name`;
-
+            const color = document.createElement(`p`);
+            const colorClass = document.createAttribute(`class`);
+            colorClass.value = `colors`;
             const titletext = productObj.name;
             title.append(titletext);
             console.log(title)
+            color.append(productColor);
+            console.log(color)
             cartItem.appendChild(cartItemContentDescription)
             displayh2.appendChild(title);
-
-
-
+            displayp.appendChild(color)
+            const price = document.createElement(`p`);
+            const priceClass = document.createAttribute(`class`);
+            priceClass.value = `price`;
+            const productPrice = productObj.price;
+            price.append(productPrice);
+            displayp.appendChild(price);
+            const quantity = document.createElement(`p`);
+            const quantityClass = document.createAttribute(`class`);
+            quantityClass.value = `quantity`;
+            quantity.append(productQuantity);
+            displayp.appendChild(quantity);
+           console.log(quantity)
 
 
 
